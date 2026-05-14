13bb8a28f2df3ade99d30e45aea12e3595f90728
woqzxje <quynhzgaming@gmail.com>
2026-05-14 14:32:11 +0700

    Add HomeController and create index.html for home page

--- src/main/java/com/example/buoi_9/controllers/HomeController.java
+++ src/main/java/com/example/buoi_9/controllers/HomeController.java
@@ -1,12 +1,13 @@
 package com.example.buoi_9.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping ("/") // b2 tạo đường link trang chủ
-    public String index(Model model){
-        
+    public String index(Model model){   //b3
+        return "pages/home/index"; //b4 trả về đường dẫn đến file html
     }
 }
