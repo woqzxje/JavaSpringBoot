94b574e41da60094f7ffc076b74c146e72a52b97
woqzxje <quynhzgaming@gmail.com>
2026-05-14 15:03:42 +0700

    Add new pages for cart, about, and shop; update HomeController for new routes

--- src/main/java/com/example/buoi_9/controllers/HomeController.java
+++ src/main/java/com/example/buoi_9/controllers/HomeController.java
@@ -6,8 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping ("/") // b2 tạo đường link trang chủ
+    @GetMapping ("/index.html") // b2 tạo đường link trang chủ
     public String index(Model model){   //b3
         return "pages/home/index"; //b4 trả về đường dẫn đến file html
+    }
+
+    @GetMapping("/cart.html")
+    public String cart(Model model) {
+        return "pages/home/cart";
+    }
+
+    @GetMapping("/about.html")
+    public String about(Model model) {
+        return "pages/home/about";
+    }
+
+    @GetMapping("/shop.html")
+    public String shop(Model model) {
+        return "pages/home/shop";
     }
 }
